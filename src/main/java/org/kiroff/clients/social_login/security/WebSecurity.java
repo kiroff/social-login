30bb2ace4797a7c964bd3c8bbb1dd9be4d315aa7
Boyan G. Kiroff <boyan.kiroff@brandmaker.com>
2026-02-25 18:15:15 +0200

    Enhance OAuth2 logout functionality and configuration. Add support for multiple identity providers, update application settings to use environment variables, and improve UI with logout link.

--- src/main/java/org/kiroff/clients/social_login/security/WebSecurity.java
+++ src/main/java/org/kiroff/clients/social_login/security/WebSecurity.java
@@ -1,16 +1,23 @@
 package org.kiroff.clients.social_login.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity
 {
+
+    @Autowired
+    ClientRegistrationRepository clientRegistrationRepository;
+
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception
     {
@@ -19,8 +26,20 @@ public class WebSecurity
                 .anyRequest().authenticated()
             )
             .oauth2Login(o -> o.defaultSuccessUrl("/home", true))
-            .logout(l -> l.logoutSuccessUrl("/"));
+            .logout(l ->
+                    //l.logoutSuccessUrl("/")
+                    l.logoutSuccessHandler(oidcLogoutSuccessHandler())
+                    .invalidateHttpSession(true)
+                    .clearAuthentication(true)
+                    .deleteCookies("JSESSIONID"));
 
         return http.build();
+    }
+
+    private OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler()
+    {
+        final OidcClientInitiatedLogoutSuccessHandler oidcClientInitiatedLogoutSuccessHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        oidcClientInitiatedLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}/xyu");//Not working (yet)
+        return oidcClientInitiatedLogoutSuccessHandler;
     }
 }
